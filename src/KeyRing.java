7515747adbb444d0bbdd9864c2101f1fd86cb263
Fotis Anthopoulos <f.anthopoulos@gmail.com>
2017-10-24 22:39:27 +0300

    yeap

--- src/KeyRing.java
+++ src/KeyRing.java
@@ -1,23 +1,45 @@
 
 public class KeyRing {
-	private int i1, i2, i3, i4, i5, i6;
-	private char[] alphabet;
+	private int i1, i2, i3, i4, i5, i6, h1, h2, h3;
+	//private char[] alphabet;
 	private String currentkey;
 
 	public KeyRing() {
-		this.i1 = 1;
+		this.h1 = 1;
+		this.h2 = 11;
+		this.h3 = 20;
 		this.i2 = 1;
 		this.i3 = 1;
 		this.i4 = 1;
 		this.i5 = 1;
 		this.i6 = 1;
-		this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+		//this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
 	}
 
-	public String getKey(){
-		currentkey = getCharForNumber(i1) + getCharForNumber(i2) + getCharForNumber(i3) +
+	public String getKey(String name){
+		int curr;
+		if(name.contentEquals("h1")) {
+			curr = h1;
+			if(i2+i3+i4+i5+i6 == 45) h1++;
+			if(h1 == 11) return "End";
+		}
+		else if(name.contentEquals("h2")) {
+			curr = h2;
+			if(i2+i3+i4+i5+i6 == 45) h2++;
+			if(h2 == 20) return "End";
+		}
+		else if(name.contentEquals("h3")) {
+			curr = h3;
+			if(i2+i3+i4+i5+i6 == 45) h3++;
+			if(h3 == 27) return "End";
+		}
+		else {
+			System.out.println("KeyRing Failure-Not Recognising Helper");
+			return null;
+		}
+		currentkey = getCharForNumber(curr) + getCharForNumber(i2) + getCharForNumber(i3) +
 				getCharForNumber(i4) + getCharForNumber(i5) + getCharForNumber(i6);
-		if(incrementKey() == 1) return "Done";
+		if(incrementKey() == 1) return "Done";//! to helpers
 		return currentkey;
 	}
 
@@ -26,7 +48,7 @@ public class KeyRing {
 			System.out.println("Increment key == null first check");
 			return 1; 
 		}
-		
+
 		i6++;
 		if(i6>26){
 			i6 = 1;
--- src/VigenereBF.java
+++ src/VigenereBF.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.util.ArrayList;
+
 import javax.swing.*;
 
 public class VigenereBF {
@@ -10,58 +12,141 @@ public class VigenereBF {
 	private JFrame frame;
 	private Container contentPane;
 	private JTextField progressTextField;
+	private Helper h1, h2, h3;
+
 
 	public VigenereBF (String ciphertext, String plaintext) {
-		super();
 		this.tries = 0;
 		this.ciphertext = ciphertext;
 		this.d = new Decryption(ciphertext);
 		this.kr = new KeyRing();
 		this.plaintext = plaintext;
 		init_gui();
+
 	}
 
 	private void init_gui(){
-		
 
-		    frame = new JFrame();
-		    frame.setBounds(200, 200, 600, 200);
-		    frame.setTitle("Attack Progress");
-		    
-		    contentPane = frame.getContentPane();
-		    contentPane.setLayout(new BorderLayout());
-		    
-		    progressTextField = new JTextField("");
-		    contentPane.add(progressTextField);
-		    
 
-		    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		    frame.setVisible(true);
-		
-		
+		frame = new JFrame();
+		frame.setBounds(200, 200, 600, 200);
+		frame.setTitle("Attack Progress");
+
+		contentPane = frame.getContentPane();
+		contentPane.setLayout(new BorderLayout());
+
+		progressTextField = new JTextField("");
+		contentPane.add(progressTextField);
+
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+
 	}
 
 	public void decode(){
-		
-		while(true){
-			if(this.oneTry() == 1) break;
+		this.h1 = new Helper("h1", plaintext, ciphertext, kr, d, this);
+		this.h2 = new Helper("h2", plaintext, ciphertext, kr, d, this);
+		this.h3 = new Helper("h3", plaintext, ciphertext, kr, d, this);
+
+
+
+
+	}
+
+	public void updateTextField() {
+		tries++;
+		progressTextField.setText("Progress: "+tries+ "/308915776 \n"+ (308915776-tries)+ " Remaining.");
+	}
+	public void decryptionComplete(String key) {
+		progressTextField.setText("Key Found: " + key + " at " + tries + " tries.");
+		System.out.println("Key Found: " + key + " at " + tries + " tries.");
+		try {
+			h1.stops();
+		}
+		finally {
+			try {
+				h2.stops();
+			}
+			finally {
+
+
+				try {
+					h3.stops();
+				}
+				finally {
+					progressTextField.setSelectionColor(Color.RED);
+				}
+			}
 		}
 	}
 
-	public int oneTry(){
-		String akey = kr.getKey();
-		if(akey.equals("Done")) return 1;
-		if(d.decrypt(akey).equals(plaintext)){
-			progressTextField.setText("Key Found: " + akey + " at " + tries + " tries.");
-			System.out.println("Key Found: " + akey + " at " + tries + " tries.");
-			return 1;
-		}
-		tries++;
-		progressTextField.setText("Progress: "+tries+ "/308915776 \n"+ (308915776-tries)+ " Remaining.");
+	public void helperEnd(Helper helper) {
+		helper.stops();
+	}
 
-		return 0;
+
+
+
+
+}
+
+class Helper implements Runnable{
+
+	private String name;
+	private String plaintext;
+	//private String ciphertext;
+	private KeyRing kr;
+	private Decryption d;
+	private VigenereBF vigi;
+	private boolean flag;
+	private Thread t;
+
+	public Helper(String name, String plaintext, String ciphertext, KeyRing kr, Decryption d, VigenereBF vigi) {
+		super();
+		flag = false;
+		this.name = name;
+		this.plaintext = plaintext;
+		//this.ciphertext = ciphertext;
+		this.kr = kr;
+		this.d = d;
+		this.vigi = vigi;
+
+	}
+	public void threadStart() {
+		t = new Thread(this, name);
+		t.start();
+	}
+
+
+	@Override
+	public void run() {
+		while(true){
+			if(flag == false)
+				oneTry();
+			else return;
+		}
+
+
+	}
+	public void stops() {
+		flag = true;//signals when helper must stop
+	}
+
+
+	public void oneTry(){
+		String akey = kr.getKey(this.name);
+		if(akey.equals("End")) flag = true;
+		if(akey.equals("Done")) vigi.decryptionComplete(akey);
+		if(d.decrypt(akey).equals(plaintext)){
+			vigi.decryptionComplete(akey);
+			flag = true;
+		}
+		vigi.updateTextField();
+		flag = false;
 	}	
-	
+
 }
 
 
